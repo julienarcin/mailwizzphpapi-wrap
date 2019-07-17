ff942cb14f76884bdcc9ee80a9112fa00cae8ecb
Julien Arcin <julien.arcin@gmail.com>
2019-07-17 14:02:19 +0700

    Update mw subscriber api to handle custom fields + delete

--- src/Subscribers.php
+++ src/Subscribers.php
@@ -14,51 +14,12 @@ $app->group('/subscribers', function() use ($app){
             $app->stop();
         }
 
-        if(!isset($post['fname']))
-        {
-            $post['fname'] = '';
-        }
-        
-        if(!isset($post['lname']))
-        {
-            $post['lname'] = '';
+        $rebuildPost = [];
+        foreach($post as $key => $value) {
+            $rebuildPost[strtoupper($key)] = $value;
         }
 
-        if(!isset($post['joindate']))
-        {
-            $post['joindate'] = '';
-        }
-
-        if(!isset($post['facebook']))
-        {
-            $post['facebook'] = '';
-        }
-
-        if(!isset($post['gender']))
-        {
-            $post['gender'] = '';
-        }
-
-        if(!isset($post['fullname']))
-        {
-            $post['fullname'] = '';
-        }
-
-        if(!isset($post['birthday']))
-        {
-            $post['birthday'] = '';
-        }
-
-        $response = $endpoint->createUpdate($post['list'], array(
-            'EMAIL'    => $post['email'],
-            'FNAME'    => $post['fname'],
-            'LNAME'    => $post['lname'],
-            'joindate'    => $post['joindate'],
-            'facebook'    => $post['facebook'],
-            'gender'    => $post['gender'],
-            'fullname'    => $post['fullname'],
-            'birthday'    => $post['birthday'],
-        ));
+        $response = $endpoint->createUpdate($post['list'], $rebuildPost);
         echo MailWizzApi_Json::encode($response->body);
     });
 
@@ -78,5 +39,20 @@ $app->group('/subscribers', function() use ($app){
         echo MailWizzApi_Json::encode($response->body);
     });
     
+    $app->post('/user/delete', function() use ($app) {
+
+        $endpoint = new MailWizzApi_Endpoint_ListSubscribers();
+
+        $post = $app->request->post();
+
+        if(!$post['email'] || !$post['list'] )
+        {
+            echo json_encode(array('status' => 'error', 'result' => 'Some parameters are missing'));
+            $app->stop();
+        }
+
+        $response = $endpoint->deleteByEmail($post['list'], $post['email']);
+        echo MailWizzApi_Json::encode($response->body);
+    });
 
 });
